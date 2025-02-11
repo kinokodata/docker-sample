2aa2f5d573a13d616c61897c751e248fd71d6f65
hyperkinoko <github.hyperkinoko@kinokodata.net>
2025-02-11 23:14:54 +0900

    コンテナ側のポート番号を変更

--- src/app.py
+++ src/app.py
@@ -14,4 +14,4 @@ def hello():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=28000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
